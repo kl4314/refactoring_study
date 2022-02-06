0ff3fedab1494e7d73c03caf5162bc3e4606480e
mjay <mjay@ryencatchers.com>
2022-02-06 19:50:12 +0900

    fix : rename 'perf' variable to aPerformance, 'volumeCredits' to result in 'volumeCreditsFor' function fix : extract 'usd' function

--- ch_01/ex/js/app.js
+++ ch_01/ex/js/app.js
@@ -29,30 +29,33 @@ function statement(invoice, plays){
         return result; // 함수 안에서 값이 바뀌는 변수 반환
     }
 
-    function volumeCreditsFor(perf){
-        let volumeCredits = 0;
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        if ("comedy" === playFor(perf).type)
-            volumeCredits += Math.floor(perf.audience / 5);
-        return volumeCredits;
+    function volumeCreditsFor(aPerformance){
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === playFor(aPerformance).type)
+            result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
+
+    function usd(aNumber){
+        return new Intl.NumberFormat("en-US",
+            {
+                style: "currency", currency : "USD",
+                minimumFractionDigits : 2 }).format(aNumber/100);
     }
 
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
-    const format = new Intl.NumberFormat("en-US",
-        {
-            style: "currency", currency : "USD",
-            minimumFractionDigits : 2 }).format;
 
     for(let perf of invoice.performances){
 
         volumeCredits += volumeCreditsFor(perf);
         // 청구 내역을 출력한다.
-        result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `총액: ${format(totalAmount/100)}\n`;
+    result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트 : ${volumeCredits}점\n`;
     return result;
 }
