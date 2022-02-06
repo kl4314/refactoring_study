3e14a252b93d9a1db18c89780582d54ca37014bc
mjay <mjay@ryencatchers.com>
2022-02-06 19:56:10 +0900

    fix : separate for loop

--- ch_01/ex/js/app.js
+++ ch_01/ex/js/app.js
@@ -55,6 +55,9 @@ function statement(invoice, plays){
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
+    for(let perf of invoice.performances){
+        volumeCredits += volumeCreditsFor(perf);
+    }
     result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트 : ${volumeCredits}점\n`;
     return result;
