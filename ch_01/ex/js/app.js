e1ae3b1d3fdf40a552eff2c855977a2070d84026
mjay <mjay@ryencatchers.com>
2022-02-06 19:04:41 +0900

    fix :  extract 'volumeCreditsFor' function

--- ch_01/ex/js/app.js
+++ ch_01/ex/js/app.js
@@ -9,7 +9,7 @@ function statement(invoice, plays){
     function amountFor(aPerformance){   // 값이 바뀌지 않는 변수는 매개변수로 전달
         let result = 0; // 변수를 초기화 하는 코드
 
-        switch (playFor(aPerformance)){
+        switch (playFor(aPerformance).type){
             case "tragedy": // 비극
                 result = 40000;
                 if(aPerformance.audience > 30){
@@ -29,9 +29,16 @@ function statement(invoice, plays){
         return result; // 함수 안에서 값이 바뀌는 변수 반환
     }
 
+    function volumeCreditsFor(perf){
+        let volumeCredits = 0;
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        if ("comedy" === playFor(perf).type)
+            volumeCredits += Math.floor(perf.audience / 5);
+        return volumeCredits;
+    }
+
     let totalAmount = 0;
     let volumeCredits = 0;
-
     let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
     const format = new Intl.NumberFormat("en-US",
         {
@@ -39,15 +46,11 @@ function statement(invoice, plays){
             minimumFractionDigits : 2 }).format;
 
     for(let perf of invoice.performances){
-        let thisAmount = amountFor(perf);
-        // 포인트를 적립한다.
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
+        volumeCredits += volumeCreditsFor(perf);
         // 청구 내역을 출력한다.
-        result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
-        totalAmount += thisAmount;
+        result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
+        totalAmount += amountFor(perf);
     }
     result += `총액: ${format(totalAmount/100)}\n`;
     result += `적립 포인트 : ${volumeCredits}점\n`;
