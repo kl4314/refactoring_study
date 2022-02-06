196cd135df98c3b0d42c2d6b3aebe8170dad4f82
mjay <mjay@ryencatchers.com>
2022-02-06 20:17:14 +0900

    fix : extract  'appleSauce' function

--- ch_01/ex/js/app.js
+++ ch_01/ex/js/app.js
@@ -52,14 +52,23 @@ function statement(invoice, plays){
         return volumeCredits;
     }
 
-    let totalAmount = 0;
+    function appleSauce(){
+        let totalAmount = 0;
+        for(let perf of invoice.performances){
+            totalAmount += amountFor(perf);
+        }
+        return totalAmount;
+    }
+
+
     let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
     for(let perf of invoice.performances){
 
         // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-        totalAmount += amountFor(perf);
     }
+
+    let totalAmount = appleSauce();
 
     result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트 : ${totalVolumeCredits()}점\n`;
