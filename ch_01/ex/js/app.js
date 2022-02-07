abd6c1e720dc4b69d12fa6a15e6422a5f438e24e
mjay <mjay@ryencatchers.com>
2022-02-07 21:13:33 +0900

    fix : add 'statementData' parameter in 'renderPlainText' function

--- ch_01/ex/js/app.js
+++ ch_01/ex/js/app.js
@@ -60,19 +60,17 @@ function statement(invoice, plays){
         return result;
     }
 
-    function renderPlainText(invoice, plays){
+    function renderPlainText(data, invoice, plays){
         let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
         for(let perf of invoice.performances){
-
-            // 청구 내역을 출력한다.
             result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         }
         result += `총액: ${usd(totalAmount())}\n`;
         result += `적립 포인트 : ${totalVolumeCredits()}점\n`;
         return result;
     }
-
-    return renderPlainText(invoice, plays);
+    const statementData = {};
+    return renderPlainText(statementData,invoice, plays);
 
 }
 
