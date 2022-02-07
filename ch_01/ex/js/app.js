128b6c2067e95e4bb9dcf2a379a0ab2b11145cc1
mjay <mjay@ryencatchers.com>
2022-02-07 21:37:41 +0900

    fix : 'playFor' function change to middle data

--- ch_01/ex/js/app.js
+++ ch_01/ex/js/app.js
@@ -19,7 +19,7 @@ function statement(invoice, plays){
     function amountFor(aPerformance){   // 값이 바뀌지 않는 변수는 매개변수로 전달
         let result = 0; // 변수를 초기화 하는 코드
 
-        switch (playFor(aPerformance).type){
+        switch (aPerformance.play.type){
             case "tragedy": // 비극
                 result = 40000;
                 if(aPerformance.audience > 30){
@@ -34,7 +34,7 @@ function statement(invoice, plays){
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw  new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
+                throw  new Error(`알 수 없는 장르 : ${aPerformance.play.type}`);
         }
         return result; // 함수 안에서 값이 바뀌는 변수 반환
     }
@@ -42,7 +42,7 @@ function statement(invoice, plays){
     function volumeCreditsFor(aPerformance){
         let result = 0;
         result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === playFor(aPerformance).type)
+        if ("comedy" === aPerformance.play.type)
             result += Math.floor(aPerformance.audience / 5);
         return result;
     }
@@ -57,7 +57,7 @@ function statement(invoice, plays){
     function renderPlainText(data, plays){
         let result = `청구 내역 (고객명 : ${data.customer})\n`;
         for(let perf of data.performances){
-            result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+            result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         }
         result += `총액: ${usd(totalAmount())}\n`;
         result += `적립 포인트 : ${totalVolumeCredits()}점\n`;
