d9f54477f7d28b2b9e4d12e64859d9973a7993d2
mjay <mjay@ryencatchers.com>
2022-02-07 21:20:07 +0900

    fix : replace invoice to statementData

--- ch_01/ex/js/app.js
+++ ch_01/ex/js/app.js
@@ -2,6 +2,11 @@
 
 function statement(invoice, plays){
 
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances;
+    return renderPlainText(statementData, plays);
+
     function playFor(aPerformance){
         return plays[aPerformance.playID];
     }
@@ -44,34 +49,32 @@ function statement(invoice, plays){
                 minimumFractionDigits : 2 }).format(aNumber/100);
     }
 
-    function totalVolumeCredits(){
-        let result = 0;
-        for(let perf of invoice.performances){
-            result += volumeCreditsFor(perf);
-        }
-        return result;
-    }
+    function renderPlainText(data, plays){
 
-    function totalAmount(){
-        let result = 0;
-        for(let perf of invoice.performances){
-            result += amountFor(perf);
+        function totalVolumeCredits(){
+            let result = 0;
+            for(let perf of data.performances){
+                result += volumeCreditsFor(perf);
+            }
+            return result;
         }
-        return result;
-    }
 
-    function renderPlainText(data, invoice, plays){
-        let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
-        for(let perf of invoice.performances){
+        function totalAmount(){
+            let result = 0;
+            for(let perf of data.performances){
+                result += amountFor(perf);
+            }
+            return result;
+        }
+
+        let result = `청구 내역 (고객명 : ${data.customer})\n`;
+        for(let perf of data.performances){
             result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         }
         result += `총액: ${usd(totalAmount())}\n`;
         result += `적립 포인트 : ${totalVolumeCredits()}점\n`;
         return result;
     }
-    const statementData = {};
-    return renderPlainText(statementData,invoice, plays);
-
 }
 
 function readJSON(file, callback) {
