1b72e0a8046d24cfa2f88fb45ac974b5165bcd66
mjay <mjay@ryencatchers.com>
2022-02-07 22:04:54 +0900

    fix : create function to change to html format

--- ch_01/ex/js/statement.js
+++ ch_01/ex/js/statement.js
@@ -18,6 +18,23 @@ function statement(invoice, plays){
                 minimumFractionDigits: 2
             }).format(aNumber / 100);
     }
+
+    function htmlStatement(invoice, plays){
+        return renderHtml(createStatementData(invoice,plays));
+    }
+    function renderHtml(data){
+        let result = `<h1>청구 내역 (고객명 : ${data.customer})</h1>\n`;
+        result += "<table>\n";
+        result += "<tr><th>연극</th><th> 좌석 수</th><th></th></tr>";
+        for(let perf of data.performances){
+            result += ` <tr><td>${perf.play.name}</td><td>${usd(perf.amount)} (${perf.audience}석)</td>`;
+            result += `<td>${usd(perf.amount)}</td></tr>\n`;
+        }
+        result += "</table>\n";
+        result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+        result += `<p>적립 포인트 : <em>${data.totalVolumeCredits}</em>점</p>\n`;
+        return result;
+    }
 }
 
 function readJSON(file, callback) {
