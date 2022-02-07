3a39a66a39fc629e7e930932d8e4ea578fc9fab6
mjay <mjay@ryencatchers.com>
2022-02-07 21:48:22 +0900

    fix : for loop change to pipeline 'totalVolumeCredits', 'totalAmount' function

--- ch_01/ex/js/app.js
+++ ch_01/ex/js/app.js
@@ -5,6 +5,8 @@ function statement(invoice, plays){
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return renderPlainText(statementData, plays);
 
     function enrichPerformance(aPerformance){
@@ -12,6 +14,7 @@ function statement(invoice, plays){
         result.play = playFor(result);
         result.amount = amountFor(result);
         result.volumeCredits = volumeCreditsFor(result);
+
         return result;
     }
     function playFor(aPerformance){
@@ -61,24 +64,16 @@ function statement(invoice, plays){
         for(let perf of data.performances){
             result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
         }
-        result += `총액: ${usd(totalAmount())}\n`;
-        result += `적립 포인트 : ${totalVolumeCredits()}점\n`;
+        result += `총액: ${usd(data.totalAmount)}\n`;
+        result += `적립 포인트 : ${data.totalVolumeCredits}점\n`;
         return result;
-        function totalVolumeCredits(){
-            let result = 0;
-            for(let perf of data.performances){
-                result += perf.volumeCredits;
-            }
-            return result;
-        }
+    }
+    function totalVolumeCredits(data){
+        return data.performances.reduce((total, p)=> total+p.volumeCredits, 0);
+    }
 
-        function totalAmount(){
-            let result = 0;
-            for(let perf of data.performances){
-                result += perf.amount;
-            }
-            return result;
-        }
+    function totalAmount(data){
+        return data.performances.reduce((total, p)=> total+p.amount, 0);
     }
 }
 
