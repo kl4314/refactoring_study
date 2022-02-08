abd30a847dfa1b0a4ae0070e2487fb6f465d9782
mjay <mjay@ryencatchers.com>
2022-02-08 13:45:59 +0900

    fix : create 'PerformanceCalculator' class that serves as intermediate data

--- ch_01/ex/js/createStatementData.js
+++ ch_01/ex/js/createStatementData.js
@@ -1,6 +1,13 @@
 
+class PerformanceCalculator{
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+}
 
 export default function createStatementData(invoice,plays) {
+
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
@@ -10,8 +17,9 @@ export default function createStatementData(invoice,plays) {
 
 
     function enrichPerformance(aPerformance) {
+        const calculator = new PerformanceCalculator(aPerformance,playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
+        result.play = calculator.play;
         result.amount = amountFor(result);
         result.volumeCredits = volumeCreditsFor(result);
 
